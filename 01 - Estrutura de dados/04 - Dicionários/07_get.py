85075432db7e5a68f8a54f3e8e7bb0e260cf4129
João Victor <jvlimah99@gmail.com>
2024-04-02 22:05:49 -0300

    formatei

--- 01 - Estrutura de dados/04 - Dicionários/07_get.py
+++ 01 - Estrutura de dados/04 - Dicionários/07_get.py
@@ -8,7 +8,5 @@ print(resultado)
 resultado = contatos.get("chave", {})  # {}
 print(resultado)
 
-resultado = contatos.get(
-    "guilherme@gmail.com", {}
-)  # {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}
+resultado = contatos.get("guilherme@gmail.com", {})  # {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}
 print(resultado)
